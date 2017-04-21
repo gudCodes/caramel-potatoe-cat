cb50bf3042e5e6e7d619d1c6edb9c79d6b95f918
MurphyMarkW <murphy.mark.w+github@gmail.com>
2017-04-21 15:37:38 -0500

    add nucleic acid sequence parsers

--- src/fasta.rs
+++ src/fasta.rs
@@ -1,3 +1,5 @@
+use std::str::from_utf8;
+
 use nom::not_line_ending;
 
 #[derive(Debug,PartialEq,Eq)]
@@ -51,7 +53,44 @@ pub enum AminoAcidCode {
     Gap, // gap of indeterminate length
 }
 
-named!(description, preceded!(char!('>'), not_line_ending));
+fn str_to_nucleic_acid_code(s: &str) -> Result<NucleicAcidCode, &str> {
+    match s.to_uppercase().as_ref() {
+        "A" => Ok(NucleicAcidCode::A), // adenosine
+        "C" => Ok(NucleicAcidCode::C), // cytidine
+        "G" => Ok(NucleicAcidCode::G), // guanine
+        "T" => Ok(NucleicAcidCode::T), // thymidine
+        "N" => Ok(NucleicAcidCode::N), // A/G/C/T (any)
+        "U" => Ok(NucleicAcidCode::U), // uridine
+        "K" => Ok(NucleicAcidCode::K), // G/T (keto)
+        "S" => Ok(NucleicAcidCode::S), // G/C (strong)
+        "Y" => Ok(NucleicAcidCode::Y), // T/C (pyrimidine)
+        "M" => Ok(NucleicAcidCode::M), // A/C (amino)
+        "W" => Ok(NucleicAcidCode::W), // A/T (weak)
+        "R" => Ok(NucleicAcidCode::R), // G/A (purine)
+        "B" => Ok(NucleicAcidCode::B), // G/T/C
+        "D" => Ok(NucleicAcidCode::D), // G/A/T
+        "H" => Ok(NucleicAcidCode::H), // A/C/T
+        "V" => Ok(NucleicAcidCode::V), // G/C/A
+        "-" => Ok(NucleicAcidCode::Gap), // gap of indeterminate length
+        _ => Err("unknown nucleic acid code '{}'"),
+    }
+}
+
+named!(description,
+    preceded!(char!('>'), not_line_ending)
+);
+
+named!(nucleic_acid_str(&[u8]) -> &str,
+    map_res!(take!(1), from_utf8)
+);
+
+named!(nucleic_acid_code(&[u8]) -> NucleicAcidCode,
+    map_res!(nucleic_acid_str, str_to_nucleic_acid_code)
+);
+
+named!(nucleic_acid_sequence(&[u8]) -> Vec<NucleicAcidCode>,
+    many0!(nucleic_acid_code)
+);
 
 #[cfg(test)]
 mod tests {
@@ -62,13 +101,70 @@ mod tests {
 
     #[test]
     fn test_description() {
-        let res = description(&b">abc 123"[..]);
-        assert_eq!(res, Done(&b""[..], &b"abc 123"[..]));
+        let data = &b">ERR001275.1198"[..];
+
+        let res = description(data);
+        assert_eq!(res, Done(&b""[..], &data[1..]));
+    }
+
+    #[test]
+    fn test_nucleic_acid_str() {
+        let data = &b"A"[..];
+
+        let res = nucleic_acid_str(data);
+        assert_eq!(res, Done(&b""[..], "A"));
+    }
+
+    #[test]
+    fn test_nucleic_acid_code() {
+        let data = &b"A"[..];
+
+        let res = nucleic_acid_code(data);
+        assert_eq!(res, Done(&b""[..], NucleicAcidCode::A));
+    }
+
+    #[test]
+    fn test_nucleic_acid_sequence() {
+        let data = &b"ACTG"[..];
+
+        let res = nucleic_acid_sequence(data);
+        assert_eq!(res, Done(&b""[..], vec![
+            NucleicAcidCode::A,
+            NucleicAcidCode::C,
+            NucleicAcidCode::T,
+            NucleicAcidCode::G,
+        ]));
     }
 
     #[bench]
     fn bench_description(b: &mut test::Bencher) {
-        b.bytes = 8;
-        b.iter(|| description(&b">abc 123"[..]));
+        let data = include_bytes!("../data/bench_description.fa");
+
+        b.bytes = data.len() as u64;
+        b.iter(|| description(data));
+    }
+
+    #[bench]
+    fn bench_nucleic_acid_str(b: &mut test::Bencher) {
+        let data = &b"A"[..];
+
+        b.bytes = 1;
+        b.iter(|| nucleic_acid_str(data));
+    }
+
+    #[bench]
+    fn bench_nucleic_acid_code(b: &mut test::Bencher) {
+        let data = &b"A"[..];
+
+        b.bytes = 1;
+        b.iter(|| nucleic_acid_code(data));
+    }
+
+    #[bench]
+    fn bench_nucleic_acid_sequence(b: &mut test::Bencher) {
+        let data = &b"ACTG"[..];
+
+        b.bytes = 4;
+        b.iter(|| nucleic_acid_sequence(data));
     }
 }
