303f0a69abcd6671e0dbac36fd8146d8fb720e71
MurphyMarkW <murphy.mark.w+github@gmail.com>
2017-04-21 12:56:28 -0500

    add boilerplate for validate command

--- src/bin/fasta.rs
+++ src/bin/fasta.rs
@@ -1,7 +1,31 @@
 #[macro_use]
+extern crate log;
+extern crate env_logger;
+
+#[macro_use]
 extern crate clap;
 
+use std::process;
+
+
+fn validate(_: &clap::ArgMatches) -> Result<(), ()> {
+    info!("I haven't a brain with which to think...");
+    Ok(())
+}
+
 fn main() {
+    env_logger::init().unwrap();
+
     let yaml = load_yaml!("fasta.yml");
     let matches = clap::App::from_yaml(yaml).get_matches();
+
+    let res = match matches.subcommand() {
+        ("validate", Some(m)) => validate(m),
+        _ => Err(()),
+    };
+
+    match res {
+        Err(_) => process::exit(1),
+        Ok(_) => {},
+    }
 }
