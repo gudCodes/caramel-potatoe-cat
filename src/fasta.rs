6721b0eff6d6767198dbe5c5e9bf7609d57e32df
MurphyMarkW <murphy.mark.w+github@gmail.com>
2017-04-21 14:10:19 -0500

    add nucleic and amino acid code enums

--- src/fasta.rs
+++ src/fasta.rs
@@ -1,5 +1,56 @@
 use nom::not_line_ending;
 
+#[derive(Debug,PartialEq,Eq)]
+pub enum NucleicAcidCode {
+    A, // adenosine
+    C, // cytidine
+    G, // guanine
+    T, // thymidine
+    N, // A/G/C/T (any)
+    U, // uridine
+    K, // G/T (keto)
+    S, // G/C (strong)
+    Y, // T/C (pyrimidine)
+    M, // A/C (amino)
+    W, // A/T (weak)
+    R, // G/A (purine)
+    B, // G/T/C
+    D, // G/A/T
+    H, // A/C/T
+    V, // G/C/A
+    -, // gap of indeterminate length
+}
+
+#[derive(Debug,PartialEq,Eq)]
+pub enum AminoAcidCode {
+    A, // alanine
+    P, // proline
+    B, // aspartate/asparagine
+    Q, // glutamine
+    C, // cystine
+    R, // arginine
+    D, // aspartate
+    S, // serine
+    E, // glutamate
+    T, // threonine
+    F, // phenylalanine
+    U, // selenocysteine
+    G, // glycine
+    V, // valine
+    H, // histidine
+    W, // tryptophan
+    I, // isoleucine
+    Y, // tyrosine
+    K, // lysine
+    Z, // glutamate/glutamine
+    L, // leucine
+    X, // any
+    M, // methionine
+    N, // asparagine
+    *, // translation stop
+    -, // gap of indeterminate length
+}
+
 named!(description, preceded!(char!('>'), not_line_ending));
 
 #[cfg(test)]
